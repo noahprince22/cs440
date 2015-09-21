2fbaad473ee252214cbee42fb9c652c62f804344
Kristofir <kristofir.wan@gmail.com>
2015-09-20 20:36:33 -0500

    Add files

--- mp1/parse.py
+++ mp1/parse.py
@@ -6,14 +6,14 @@ logging.basicConfig(filename='happenings.log', level=logging.DEBUG)
 def anyone_home_at(x,y):
     if x < 0 or x >= maze_width:
         return False
-    if y < 0 and y >= maze_height:
+    if y < 0 or y >= maze_height:
         return False
     return True
 
 def find(char):
     for y in range(1,maze_height):
         for x in range(1,maze_width):
-            if whats_at(x,y) is char:
+            if maze[y][x] is char:
                 return (x,y)
                 # "+1" accounting for x,y starting at 0
 
@@ -27,7 +27,7 @@ def manhattan_distance(begin, end):
     y = abs(begin[1] - end[1])
     return (x + y)
 
-raw_maze = [list(char for char in line.rstrip('\n')) for line in open('maze.txt')]
+maze = [list(char for char in line.rstrip('\n')) for line in open('maze.txt')]
 maze_width = len(maze[0])
 maze_height = len(maze)
 start = find("P")
@@ -35,20 +35,6 @@ goal = find(".")
 
 maze_walkable_bool = [[is_walkable(x, y) for x in range(0,maze_width)] for y in range(0,maze_height)]
 
-maze_nodes_filtered = []
+state = start
 
-for row in maze_nodes:
-    for node in row:
-        x = node[1]
-        y = node[0]
-        if maze_walkable_bool[y][x] is True:
-            maze_nodes_filtered.append(node)
-
-# for y in maze_walkable_bool:
-#     for x in y:
-#         if x is True:
-#             sys.stdout.write(' ')
-#         else:
-#             sys.stdout.write('%')
-#     print("")
-# This nearly replicates the original input maze. This proves that the 2D array is accurate.
+#while state is not goal
